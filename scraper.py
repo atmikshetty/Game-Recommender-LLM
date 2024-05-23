1810725f396fa01243914c79af79cda2ddc9bd2b
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-05-23 11:45:16 +0530

    Data collection complete

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-from google_play_scraper import search, Sort
+from google_play_scraper import search
+import csv
 import json
 
 results = search(
-    "Trending games",
+    "Best games of 2023-2024",
     lang="en",
     country="us",
     n_hits=30  # max limit
@@ -11,18 +12,26 @@ results = search(
 
 def extract_req_info(game):
     return {
-        'game_name': game.get('title', ''),
+        'name': game.get('title', ''),
         'package_name': game.get('appId', ''),
         'description': game.get('description', ''),
         'icon_url': game.get('icon', ''),
         'genre': game.get('genre', ''),
         'rating': game.get('score', ''),
-        'no_of_ratings': game.get('installs', '')
+        'ratings': game.get('installs', '')
     }
 
 extracted_info = [extract_req_info(game) for game in results]
 
-for info in extracted_info:
-    print(json.dumps(info, indent=4))
+# for info in extracted_info:
+#     print(json.dumps(info, indent=7))
 
-print(len(extracted_info))
+file = 'best_games.csv'
+headers = ['name', 'package_name', 'description', 'icon_url', 'genre', 'rating', 'ratings']
+
+with open(file, 'w', newline='', encoding='utf-8') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(extracted_info)
+
+print("Successfully stored data into csv!")
